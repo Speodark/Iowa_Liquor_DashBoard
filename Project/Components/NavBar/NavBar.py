32d42c4bc181f58d912bbd6c9419712e258c15a2
Matan Morduch <matanmor9@gmail.com>
2022-03-26 23:31:38 +0300

    worked on the header colors and functionality with sass, the individual links can be hovered and there is a nice animation, plus we have the nice kind of icon which is used for spacing

--- Project/Components/NavBar/NavBar.py
+++ Project/Components/NavBar/NavBar.py
@@ -1,7 +1,38 @@
-from dash import html
+from dash import html, dcc
 
 
 def navbar():
     return html.Div(
         className="navbar",
+        children=[
+            html.H3(
+                className="navbar__item navbar__header heading-4",
+                children=html.P("Iowa Liquor")
+            ),
+            html.Div(
+                className="navbar__icons-container",
+                children=[
+                    html.Div(),
+                    html.Div(),
+                    html.Div(),
+                    html.Div(),
+                    html.Div()
+                ]
+            ),
+            dcc.Link(
+                className="navbar__item navbar__link",
+                children=html.P("Sales"),
+                href="/Sales"
+            ),
+            dcc.Link(
+                className="navbar__item navbar__link",
+                children=html.P("Inventory"),
+                href="/Inventory"
+            ),
+            dcc.Link(
+                className="navbar__item navbar__link",
+                children=html.P("Page"),
+                href="/Page"
+            ),
+        ]
     )
